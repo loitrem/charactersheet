b5019e1cbdc376cd09c55f4153e5e5389b2d9594
loitrem <ski_fix@hotmail.com>
2022-04-26 12:19:53 -0400

    CharacterSkills model updated

--- src/main/java/com/loitrem/CharacterSheet/models/CharacterSkills.java
+++ src/main/java/com/loitrem/CharacterSheet/models/CharacterSkills.java
@@ -28,12 +28,319 @@ public class CharacterSkills {
     Characters csCharacters;
 
     @NotBlank
-    int csAbilityMod;
+    int csAcrobaticsAbilityMod;
 
     @NotBlank
-    int csRanks;
+    int csAcrobaticsRanks;
 
     @NotBlank
-    int csMiscMod;
+    int csAcrobaticsMiscMod;
+
+    @NotBlank
+    int csAppraiseAbilityMod;
+
+    @NotBlank
+    int csAppraiseRanks;
+
+    @NotBlank
+    int csAppraiseMiscMod;
+
+    @NotBlank
+    int csBluffAbilityMod;
+
+    @NotBlank
+    int csBluffRanks;
+
+    @NotBlank
+    int csBluffMiscMod;
+
+    @NotBlank
+    int csClimbAbilityMod;
+
+    @NotBlank
+    int csClimbRanks;
+
+    @NotBlank
+    int csClimbMiscMod;
+
+    @NotBlank
+    int csCraftAbilityMod;
+
+    @NotBlank
+    int csCraftRanks;
+
+    @NotBlank
+    int csCraftMiscMod;
+
+    @NotBlank
+    int csDiplomacyAbilityMod;
+
+    @NotBlank
+    int csDiplomacyRanks;
+
+    @NotBlank
+    int csDiplomacyMiscMod;
+
+    @NotBlank
+    int csDisableDeviceAbilityMod;
+
+    @NotBlank
+    int csDisableDeviceRanks;
+
+    @NotBlank
+    int csDisableDeviceMiscMod;
+
+    @NotBlank
+    int csDisguiseAbilityMod;
+
+    @NotBlank
+    int csDisguiseRanks;
+
+    @NotBlank
+    int csDisguiseMiscMod;
+
+    @NotBlank
+    int csEscapeArtistAbilityMod;
+
+    @NotBlank
+    int csEscapeArtistRanks;
+
+    @NotBlank
+    int csEscapeArtistMiscMod;
+
+    @NotBlank
+    int csFlyAbilityMod;
+
+    @NotBlank
+    int csFlyRanks;
+
+    @NotBlank
+    int csFlyMiscMod;
+
+    @NotBlank
+    int csHandleAnimalAbilityMod;
+
+    @NotBlank
+    int csHandleAnimalRanks;
+
+    @NotBlank
+    int csHandleAnimalMiscMod;
+
+    @NotBlank
+    int csHealAbilityMod;
+
+    @NotBlank
+    int csHealRanks;
+
+    @NotBlank
+    int csHealMiscMod;
+
+    @NotBlank
+    int csIntimidateAbilityMod;
+
+    @NotBlank
+    int csIntimidateRanks;
+
+    @NotBlank
+    int csIntimidateMiscMod;
+
+    @NotBlank
+    int csKnowledgeArcanaAbilityMod;
+
+    @NotBlank
+    int csKnowledgeArcanaRanks;
+
+    @NotBlank
+    int csKnowledgeArcanaMiscMod;
+
+    @NotBlank
+    int csKnowledgeDungeoneeringAbilityMod;
+
+    @NotBlank
+    int csKnowledgeDungeoneeringRanks;
+
+    @NotBlank
+    int csKnowledgeDungeoneeringMiscMod;
+
+    @NotBlank
+    int csKnowledgeEngineeringAbilityMod;
+
+    @NotBlank
+    int csKnowledgeEngineeringRanks;
+
+    @NotBlank
+    int csKnowledgeEngineeringMiscMod;
+
+    @NotBlank
+    int csKnowledgeGeographyAbilityMod;
+
+    @NotBlank
+    int csKnowledgeGeographyRanks;
+
+    @NotBlank
+    int csKnowledgeGeographyMiscMod;
+
+    @NotBlank
+    int csKnowledgeHistoryAbilityMod;
+
+    @NotBlank
+    int csKnowledgeHistoryRanks;
+
+    @NotBlank
+    int csKnowledgeHistoryMiscMod;
+
+    @NotBlank
+    int csKnowledgeLocalAbilityMod;
+
+    @NotBlank
+    int csKnowledgeLocalRanks;
+
+    @NotBlank
+    int csKnowledgeLocalMiscMod;
+
+    @NotBlank
+    int csKnowledgeNatureAbilityMod;
+
+    @NotBlank
+    int csKnowledgeNatureRanks;
+
+    @NotBlank
+    int csKnowledgeNatureMiscMod;
+
+    @NotBlank
+    int csKnowledgeNobilityAbilityMod;
+
+    @NotBlank
+    int csKnowledgeNobilityRanks;
+
+    @NotBlank
+    int csKnowledgeNobilityMiscMod;
+
+    @NotBlank
+    int csKnowledgePlanesAbilityMod;
+
+    @NotBlank
+    int csKnowledgePlanesRanks;
+
+    @NotBlank
+    int csKnowledgePlanesMiscMod;
+
+    @NotBlank
+    int csKnowledgeReligionAbilityMod;
+
+    @NotBlank
+    int csKnowledgeReligionRanks;
+
+    @NotBlank
+    int csKnowledgeReligionMiscMod;
+
+    @NotBlank
+    int csLinguisticsAbilityMod;
+
+    @NotBlank
+    int csLinguisticsRanks;
+
+    @NotBlank
+    int csLinguisticsMiscMod;
+
+    @NotBlank
+    int csPerceptionAbilityMod;
+
+    @NotBlank
+    int csPerceptionRanks;
+
+    @NotBlank
+    int csPerceptionMiscMod;
+
+    @NotBlank
+    int csPerformAbilityMod;
+
+    @NotBlank
+    int csPerformRanks;
+
+    @NotBlank
+    int csPerformMiscMod;
+
+    @NotBlank
+    int csProfessionAbilityMod;
+
+    @NotBlank
+    int csProfessionRanks;
+
+    @NotBlank
+    int csProfessionMiscMod;
+
+    @NotBlank
+    int csRideAbilityMod;
+
+    @NotBlank
+    int csRideRanks;
+
+    @NotBlank
+    int csRideMiscMod;
+
+    @NotBlank
+    int csSenseMotiveAbilityMod;
+
+    @NotBlank
+    int csSenseMotiveRanks;
+
+    @NotBlank
+    int csSenseMotiveMiscMod;
+
+    @NotBlank
+    int csSleightOfHandAbilityMod;
+
+    @NotBlank
+    int csSleightOfHandRanks;
+
+    @NotBlank
+    int csSleightOfHandMiscMod;
+
+    @NotBlank
+    int csSpellcraftAbilityMod;
+
+    @NotBlank
+    int csSpellcraftRanks;
+
+    @NotBlank
+    int csSpellcraftMiscMod;
+
+    @NotBlank
+    int csStealthAbilityMod;
+
+    @NotBlank
+    int csStealthRanks;
+
+    @NotBlank
+    int csStealthMiscMod;
+
+    @NotBlank
+    int csSurvivalAbilityMod;
+
+    @NotBlank
+    int csSurvivalRanks;
+
+    @NotBlank
+    int csSurvivalMiscMod;
+
+    @NotBlank
+    int csSwimAbilityMod;
+
+    @NotBlank
+    int csSwimRanks;
+
+    @NotBlank
+    int csSwimMiscMod;
+
+    @NotBlank
+    int csUseMagicDeviceAbilityMod;
+
+    @NotBlank
+    int csUseMagicDeviceRanks;
+
+    @NotBlank
+    int csUseMagicDeviceMiscMod;
+
 
 }
