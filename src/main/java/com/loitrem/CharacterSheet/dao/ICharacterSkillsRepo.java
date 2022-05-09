999c94a1fa8a9c3447dc14e7d6a46e368fe5f8e0
loitrem <ski_fix@hotmail.com>
2022-05-09 12:35:42 -0400

    added findById to CharacterSkillsService and iCharacterSkillsRepo

--- src/main/java/com/loitrem/CharacterSheet/dao/ICharacterSkillsRepo.java
+++ src/main/java/com/loitrem/CharacterSheet/dao/ICharacterSkillsRepo.java
@@ -4,6 +4,10 @@ import com.loitrem.CharacterSheet.models.CharacterSkills;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ICharacterSkillsRepo extends JpaRepository<CharacterSkills, Long> {
+
+    List<CharacterSkills> findBycskId(long id);
 }
--- src/main/java/com/loitrem/CharacterSheet/services/CharacterSkillsService.java
+++ src/main/java/com/loitrem/CharacterSheet/services/CharacterSkillsService.java
@@ -1,7 +1,26 @@
 package com.loitrem.CharacterSheet.services;
 
+import com.loitrem.CharacterSheet.dao.ICharacterSkillsRepo;
+import com.loitrem.CharacterSheet.models.CharacterSkills;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CharacterSkillsService {
+
+    ICharacterSkillsRepo iCharacterSkillsRepo;
+
+    @Autowired
+    public CharacterSkillsService(ICharacterSkillsRepo iCharacterSkillsRepo) { this.iCharacterSkillsRepo = iCharacterSkillsRepo; }
+
+    public List<CharacterSkills> findById(Long id) {
+        if (id != 0)
+        {
+            return iCharacterSkillsRepo.findBycskId(id);
+        }
+        return null;
+    }
+
 }
